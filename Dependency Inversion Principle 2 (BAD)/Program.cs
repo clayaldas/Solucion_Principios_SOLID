9a9f4c3f303fe7debf4addf74bcdb2b93318140d
Clay Aldas <clayaldas@gmail.com>
2025-03-31 20:30:49 -0500

    Creado el proyecto

---
+++ Dependency Inversion Principle 2 (BAD)/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Dependency_Inversion_Principle_2__BAD_
+{ 
+  // Módulos de alto nivel dependen directamente de módulos de bajo nivel
+  public class FrontEndDeveloper
+  {
+    public void Develop()
+    {
+      Console.WriteLine("El desarrollador front-end está codificando en React...");
+    }
+  }
+
+  public class BackEndDeveloper
+  {
+    public void Develop()
+    {
+      Console.WriteLine("El desarrollador back-end está codificando en C #...");
+    }
+  }
+
+  // Clase de alto nivel
+  public class Project
+  {
+    private readonly FrontEndDeveloper _frontend;
+    private readonly BackEndDeveloper _backend;
+
+    public Project()
+    {
+      _frontend = new FrontEndDeveloper();
+      _backend = new BackEndDeveloper();
+    }
+
+    public void Develop()
+    {
+      _frontend.Develop();
+      _backend.Develop();
+    }
+  }
+
+  class Program
+  {
+    static void Main()
+    {
+      Project project = new Project();
+      project.Develop();
+
+      Console.ReadLine();
+    }
+  }
+  
+}
